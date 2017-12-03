b8b06a6c2d0f4cbb3216d9a985c74b04d7dc75c8
FSommer <sommer.florian@gmx.de>
2017-12-03 15:50:58 +0100

    Jdoc

--- src/app/chat/model/message.model.ts
+++ src/app/chat/model/message.model.ts
@@ -1,10 +1,19 @@
-
+/**
+ * The Model of a single message within a chat.
+ * ChatComponent's rendering of messages is based on this model.
+ */
 export class Message {
   own: boolean;
   content: string;
   from: string;
   timeStamp: Date;
 
+  /**
+   * @param {string} from The username of the user who sent this message
+   * @param {string} content the actual message, that the user wants to send in the chat
+   * @param {Date} timeStamp the date & time, when the user did send the message (precision at least to the minute)
+   * @param {boolean} own meaning was this message sent by our own client - defaults to false.
+   */
   constructor(from: string, content: string, timeStamp: Date, own: boolean = false) {
     this.content = content;
     this.from = from;
